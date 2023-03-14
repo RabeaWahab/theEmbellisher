fbdba0952ff998429cc5eb74fe7af1efde76fcda
Rabea Abdelwahab <wahab.r@gmail.com>
2023-03-14 14:27:34 -0400

    use with instead of open and pick the right item from the row

--- theEmbellisher.py
+++ theEmbellisher.py
@@ -1,36 +1,35 @@
 import openai
 from dotenv import load_dotenv
 import os
+import csv
+
 
 load_dotenv()
-
 openAiKey = os.getenv("OPENAI_API_KEY")
 print(openAiKey)
 
 # read a file on the same directory line by line and print it out
-def process_file():
-    file = open("keywords.csv", "r")
-    # skip the first line
-    next(file)
-    for line in file:
-        item = returnLine(line)
-        print(item[3])
-        result = callOpenAI(item[3])
-        print(result)
-        break
-    file.close()
-
-# return a line as a tuple
-def returnLine(line):
-    line = line.split(",")
-    return line
+def processFile():
+    with open("keywords.csv") as csv_file:
+        file = csv.reader(csv_file, delimiter=',', quotechar='"')
+        # skip the first line
+        next(file)
+        i = 0
+        for line in file:
+            item = line
+            print(item[3])
+            result = callOpenAI(item[3], item[0])
+            print(result)
+            if (i > 2):
+                break
+            i+=1
 
 # call openai api to get the result from chatgpt-turbo-3.5 model    
-def callOpenAI(text: str):
+def callOpenAI(text: str, date: str):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[{
-            "content": "what is the latest on inflation in the united states?",
+            "content": "Sort the following keywords based on their societal impact, choose the top 5 keywords that you think are the most impactful if they are more than 5 and use them in a summary related to the day ({date}): {keywords}".format(date=date, keywords=text),
             "role": "user"
         }],
         temperature=0,
@@ -41,10 +40,6 @@ def callOpenAI(text: str):
         # stop=["\n", " Human:", " AI:"]
     )
     print(response)
-
-
-
-
-
-
-process_file()
+    
+    
+processFile()
